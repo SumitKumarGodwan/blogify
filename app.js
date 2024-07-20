300483c11d1474d31fe4aa8d06e8775b98e9c4b9
SumitKumarGodwan <sumitgod510@gmail.com>
2024-07-20 17:02:46 +0530

    GO-1, mongo url change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect("mongodb+srv://sumitgod510:Xz8nZJ2nzyu1NMXZ@cluster0.vsttzj8.mongodb.net/blog?retryWrites=true&w=majority")
   .then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
